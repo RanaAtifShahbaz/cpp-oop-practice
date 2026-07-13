c4fac0860850b1124b8fa19642c5fdaeba7a453b
RanaAtifShahbaz <ranaatif.dev@gmail.com>
2026-07-13 20:56:24 +0500

    Challenge 2

--- first.cpp
+++ first.cpp
@@ -34,44 +34,73 @@ using namespace std;
 // }
 
 
-class Bank{
-    private:
-    int balance;
-    int amount;
-    int remaining;
-    public:
-    void deposit(int b){
-        if(b<=0){
-            cout<<"Deposit Must Be Greater Than Zero "<<endl;
+// class Bank{
+//     private:
+//     int balance;
+//     int amount;
+//     int remaining;
+//     public:
+//     void deposit(int b){
+//         if(b<=0){
+//             cout<<"Deposit Must Be Greater Than Zero "<<endl;
         
-        }
-        balance=b;
-    }
-    void withdraw(){
+//         }
+//         balance=b;
+//     }
+//     void withdraw(){
         
-        cout<<"Enter The amount to withdraw "<<endl;
-          cin>>amount;
-        if(amount>balance){
-            cout<<"Not have enough balance to withdraw ";
-        }
+//         cout<<"Enter The amount to withdraw "<<endl;
+//           cin>>amount;
+//         if(amount>balance){
+//             cout<<"Not have enough balance to withdraw ";
+//         }
       
 
 
+//     }
+//     void display_balance (){
+
+//         remaining=balance-amount;
+//         cout<<"Your Remaining Balance is "<<remaining<<endl;
+
+
+//     }
+
+
+// };
+// int main(){
+//     Bank b1;
+//     b1.deposit(2000);
+//     b1.withdraw();
+//     b1.display_balance();
+
+// }
+
+class Student{
+    private:
+    string name;
+    int roll_number;
+    float gpa;
+    public:
+    void setdata(string s, int r,float g){
+        name=s;
+        roll_number=r;
+        gpa=g;}
+        
+    void getdata(){
+        cout<<"Roll No is "<<roll_number<<endl;
+        cout<<"Name is "<<name<<endl;
+        if(gpa<0||gpa>4){
+            cout<<"Invalid GPA Input"<<endl;
+        }
+        else
+        cout<<"GPA is "<<gpa<<endl;
     }
-    void display_balance (){
-
-        remaining=balance-amount;
-        cout<<"Your Remaining Balance is "<<remaining<<endl;
-
-
-    }
-
+    
 
 };
-int main(){
-    Bank b1;
-    b1.deposit(2000);
-    b1.withdraw();
-    b1.display_balance();
-
+int  main(){
+    Student s;
+    s.setdata("Atif",30,3.00);
+    s.getdata();
 }
